825c69ade872fae7adf8a1032c24e41e27695153
Timur Feltin <timur.feltin@gmail.com>
2024-01-29 15:39:47 +0200

    bash script yaml

--- kubernetes-application-wont-start/background.sh
+++ kubernetes-application-wont-start/background.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 kubectl create ns production
-kubectl apply -f deployment.yaml
+cat <<EOF | kubectl apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: some-prod-app
+  namespace: production
+  labels:
+    app: some-prod-app
+spec:
+  replicas: 4
+  selector:
+    matchLabels:
+      app: some-prod-app
+  template:
+    metadata:
+      labels:
+        app: some-prod-app
+    spec:
+      containers:
+        - name: some-prod-app-container
+          image: httpd:latest
+          ports:
+            - containerPort: 80
+
+EOF
